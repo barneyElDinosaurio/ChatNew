de689b076c43a3ab8949242c329615f30833d6d8
barneyElDinosaurio <marioalzatelopez@gmail.com>
2018-06-21 19:21:31 -0500

    adding nav drawer style and clicks on options

--- src/main/java/xx/xxx/xxxx/ui/MainActivity.java
+++ src/main/java/xx/xxx/xxxx/ui/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import xx.xxx.xxxx.R;
 
@@ -33,6 +34,7 @@ public class MainActivity extends Activity
      */
     private CharSequence mTitle;
 
+    ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,36 +42,35 @@ public class MainActivity extends Activity
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
+
 
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        //Set action bar
+        actionBar = getActionBar();
+        actionBar.setBackgroundDrawable((getResources().getDrawable(R.drawable.ic_action_add_person)));
+        actionBar.setDisplayShowTitleEnabled(false);
     }
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
+        switch (position){
+            case 0:
+                Toast.makeText(this, "menu1", Toast.LENGTH_SHORT).show();
+                break;
             case 1:
-                mTitle = getString(R.string.title_section1);
+                Toast.makeText(this, "menu2", Toast.LENGTH_SHORT).show();
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
+                Toast.makeText(this, "menu3", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
+
 
     public void restoreActionBar() {
         ActionBar actionBar = getActionBar();
@@ -78,44 +79,5 @@ public class MainActivity extends Activity
         actionBar.setTitle(mTitle);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
 
 }
